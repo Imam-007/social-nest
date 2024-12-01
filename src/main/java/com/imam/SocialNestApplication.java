6d1cb97aec818b88a00cf121d9bf1bab3cabc8f4
Imam <imam@IBEEs-MacBook-Air-2.local>
2024-12-01 22:13:43 +0530

    project setup

---
+++ src/main/java/com/imam/SocialNestApplication.java
@@ -0,0 +1,13 @@
+package com.imam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imam/SocialNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
